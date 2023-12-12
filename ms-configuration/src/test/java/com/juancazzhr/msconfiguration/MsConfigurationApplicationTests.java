1e93d801e526fab048e4f05f97aff8643dd30fc2
Juan Camilo Hernández Ramírez <Juancazzhr@gmail.com>
2023-12-12 02:52:12 -0500

    Initial commit

---
+++ ms-configuration/src/test/java/com/juancazzhr/msconfiguration/MsConfigurationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juancazzhr.msconfiguration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsConfigurationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
